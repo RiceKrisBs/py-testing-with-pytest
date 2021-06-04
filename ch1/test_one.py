671d8d9002104367c2f777ad188caa4c2db536e8
Kris Bucyk <krisbucyk@gmail.com>
2021-06-03 22:12:16 -0400

    Ch1 exercise - change assert

--- ch1/test_one.py
+++ ch1/test_one.py
@@ -1,5 +1,12 @@
+def _check_length(word):
+    return len(word) > 10
+
 def test_passing():
     # below will print with pytest option -s
     print('In method <test_passing>.')
+    word = "Kristopher Bucyk"
+    assert _check_length(word)
 
-    assert (1, 2, 3) == (1, 2, 3)
+def test_another_failing():
+    word = "kris"
+    assert _check_length(word)

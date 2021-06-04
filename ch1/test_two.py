8667e2e76e11f6e641cc14afb66502684ffc2beb
Kris Bucyk <krisbucyk@gmail.com>
2021-06-03 21:25:57 -0400

    local variable for pytest option -l

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,2 +1,6 @@
 def test_failing():
+    # setting a local variable to print when  test fails and pytest
+    # is executed with option -l
+    var_kris = 'Mr. Kristopher'
+
     assert (1, 2, 3) == (3, 2, 1)
